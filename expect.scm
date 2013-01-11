dbf2742f7e086366c0fc937a4babfe7a1f932660
Jason Felice <jason.m.felice@gmail.com>
2013-01-11 12:03:57 -0500

    start killin ifs, remove let

--- expect.scm
+++ expect.scm
@@ -3,31 +3,29 @@
   (define (includes-failure-message)
     (= 2 (length args)))
   
-  (define (failure-message)
-    (if (includes-failure-message)
-      (test-condition)
-      "a generic failure message"))
-
   (define (test-condition)
     (if (includes-failure-message)
       (cadr args)
       (car args)))
 
+  (define (failure-message)
+    (car args))
+
+;; (define (store-verbose-failure-message))
+
   (if (includes-failure-message) 
-    (let ((message (car args))
-          (test-expression (cadr args)))
-      `(if (not ,(test-condition))
-	 (begin
-	   (display "X")
-	   (newline)
-	   (display "TEST FAILED: ")
-	   (display ,message))
-	(display ".")))
     `(if (not ,(test-condition))
        (begin
 	 (display "X")
+	 (newline)
+	 (display "TEST FAILED: ")
+	 (display ,(failure-message)))
+      (display "."))
+    `(if (not ,(test-condition))
+       (begin
          (display "TEST FAILED: ")
-         (display ',(car args))))))
+         (display ',(car args))
+	 (display "X")))))
 
 (define (display-expect-results)
   (display "All passed")
